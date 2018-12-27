43f7f4e5758f45b4352a743cb059222eba5f0899
PangYanbo <pybdtc@gmail.com>
2018-12-27 08:15:23 +0000

    mc irl

--- Env/Network/network_based_env.py
+++ Env/Network/network_based_env.py
@@ -78,29 +78,39 @@ class Env(object):
     
     def __init__(self, G):
         self.graph = G
-        self.sub_graph = self.sub_graph(800)
-#        self.path = path
+        #self.sub_graph = self.sub_graph(800)
+
 #        self.trajectories = list(self.load_trajectory(n_demonstrations).values())
 #        self.trajectory_length = len(self.trajectories[0])
 	
         self.n_nodes = len(self.graph.nodes)
         self.n_edges = len(self.graph.edges)
 	
-        self.state_space, self.node_state = self.initial_state_space()
-        self.action_space, self.action_index = self.initial_action_space()
+       # global network
+       # self.state_space, self.node_state = self.initial_state_space()
+       # self.action_space, self.action_index = self.initial_action_space()
+
+       # partial network from trajectories
+        self.trajectories = list(self.load_trajectories('/home/ubuntu/PycharmProjects/DeepRLAgent/demonstrations/test_sub.csv', 1200).values())
+        self.state_space, self.action_space = self.init_space_from_demo() 
+        self.initial_state()
 
         self.n_states = len(self.state_space)
         self.n_actions = len(self.action_space)
 
-        self.n_features = 100
-        self.telepoint_feature = self.load_telepoint_feature('/home/ubuntu/Data/Tokyo/Telepoint/')	       self.weather_feature = self.load_weather_feature('/home/ubuntu/Data/Tokyo/Weather/')
+        self.n_features = 110
+        self.theta = np.zeros([self.n_features])
+        self.telepoint_feature = self.load_telepoint_feature('/home/ubuntu/Data/Tokyo/Telepoint/')
+        self.weather_feature = self.load_weather_feature('/home/ubuntu/Data/Tokyo/Weather/20150101.csv')
 
-        
+        self.feature_space = self.feature_dict()       
+
+
     def step(self, s, a, t):
 	# TODO
         # action.destination is a string of node
-        s_ = self.node_state[a.destination]
-        r = self.reward(s, a, t)   
+        s_ = a
+        r = self.reward((t, s, a))   
         
         done = False
         
@@ -116,11 +126,34 @@ class Env(object):
         
         return random.choice(self.state_space)
 
+    def initial_state(self):
+        initial_state = []
+
+        for traj in self.trajectories:
+            initial_state.append(traj[0][1])
+        self.initial_state = initial_state
+        print("initial state count: ", len(initial_state))
+
+
     def sub_graph(self, size):
         nodes = random.sample(self.graph.nodes, size)
         subgraph = self.graph.subgraph(nodes)
         
         return subgraph
+
+    def init_space_from_demo(self):
+        state_space = {}
+        action_space = []
+        for traj in self.trajectories:
+            for i in range(len(traj)):
+                time, s, a = traj[i]
+                if s not in state_space:
+                    state_space[s] = {a}
+                else:
+                    state_space[s].add(a)
+                action_space.append(a)
+        return state_space, action_space
+
 
     def initial_state_space(self):
         state_space = []
@@ -182,7 +215,7 @@ class Env(object):
         for i in range(len(tele_point_list)):
             filename =  tele_point_list[i]
             index = filename[:-4]
-            index_mesh_point[index] = dict()
+            index_mesh_count[index] = dict()
             with open(path+filename, 'r') as f:
                 for line in f.readlines():
                     tokens = line.strip('\n').split(',')
@@ -201,7 +234,7 @@ class Env(object):
             for line in f.readlines():
                 tokens = line.strip('\n').split(',')
                 time = int(tokens[0])
-                time_column_info = {}
+                time_column_info[time] = dict()
                 for i in range(len(tokens)-1):
                     if i < 9:
                         time_column_info[time][i] = float(tokens[i])  
@@ -214,8 +247,8 @@ class Env(object):
         output: vector(np.array([self.n_features]))
         """
         t, s, a = episode
-        origin = s.node
-        destination = a.node
+        origin = s
+        destination = a
         dest_mesh = ju.to_meshcode(self.graph.nodes[destination]['y'], self.graph.nodes[destination]['x'], 5)
 
         # path = nx.shortest_path(self.graph, origin, destination)
@@ -223,28 +256,41 @@ class Env(object):
         f = np.zeros([self.n_features])
         
         i = 0 
-        for key in self.telepoint_feature
-            if mesh in self.telepoint_feature[key]：
+        for key in self.telepoint_feature:
+            if dest_mesh in self.telepoint_feature[key]:
                 f[i] = self.telepoint_feature[key][dest_mesh] 
             i += 1             
 
-        f[i] = self.graph.nodes[origin]['x']
-        f[i+1] = self.graph.nodes[origin]['y']
-        f[i+2] = self.graph.nodes[destination]['x']
-        f[i+3] = self.graph.nodes[destination]['y']
+        #f[i] = self.graph.nodes[origin]['x']
+        #f[i+1] = self.graph.nodes[origin]['y']
+        #f[i+2] = self.graph.nodes[destination]['x']
+        #f[i+3] = self.graph.nodes[destination]['y']
         # weather data
         for j in range(8):
-            f[i+3+j] = self.weather_feature[t][j]
+            f[i+3+j] = self.weather_feature[int(t/2)][j]
         
         return f
-    
+
+    def feature_dict(self):
+        episode_features = dict()
+        for traj in self.trajectories:
+            for episode in traj:
+                if episode not in episode_features:
+                    episode_features[episode] = self.episode_feature(episode)
+        print(episode_features.keys())
+        return episode_features
+
+
     def update_reward_function(self, theta):
         self.theta = theta
 
     def reward(self, episode):
-        
-        return self.theta.dot(self.episode_feature(episode))
-
+        if episode in self.feature_space:
+            print("episode in feature space")
+            return self.theta.dot(self.feature_space[episode])
+        else:
+            print("episode not in feature space")
+            return self.theta.dot(self.episode_feature(episode))
 
     def load_episode(self, path):
 
@@ -274,3 +320,40 @@ class Env(object):
                 prev_id = uid
                 count += 1
         return episodes
+
+    def trajectory_rewards(self):
+        rewards = []
+        for traj in self.trajectories:
+            reward = sum([self.reward(e) for e in traj])
+            rewards.append(reward)
+
+        return np.average(rewards)   
+
+    def load_trajectories(self, path, number):
+        id_traj = {}
+        count = 0
+
+        with open(path, 'r') as f:
+            for line in f.readlines():
+                try:
+                    if count > number:
+                        break
+                    count += 1
+                    tokens = line.strip('\n').split(',')
+                    uid = tokens[0]
+                    time = int(tokens[1])
+                    origin = tokens[2]
+                    dest = tokens[3]
+                    # mode = tokens[4]
+                   
+                    episode = (time, origin, dest)
+                   
+                    if uid not in id_traj:
+                        traj = [episode]
+                        id_traj[uid] = traj
+                    else:
+                        id_traj[uid].append(episode)
+                except(AttributeError, ValueError, IndexError, TypeError):
+                    print('Load Trajectory Error')
+        f.close()
+        return id_traj
